ed7ed74e6bf69839928aa3334fd9af9e1e4dd4b3
razahaider03 <54476563+razahaider03@users.noreply.github.com>
2023-03-21 11:00:50 +0530

    modified:   CustomizeAdmin/myapp/admin.py 	new file:   CustomizeAdmin/myapp/migrations/0001_initial.py

--- CustomizeAdmin/myapp/admin.py
+++ CustomizeAdmin/myapp/admin.py
@@ -10,12 +10,17 @@ admin.site.register(Reservation)
 
 from django.contrib.auth.admin import UserAdmin 
 @admin.register(User) 
-class NewAdmin(UserAdmin): 
+class NewAdmin(UserAdmin):
+    readonly_fields = [ 
+            'date_joined', 
+    ] 
     def get_form(self, request, obj=None, **kwargs): 
         form = super().get_form(request, obj, **kwargs) 
         is_superuser = request.user.is_superuser 
 
         if not is_superuser: 
             form.base_fields['username'].disabled = True 
+            form.base_fields['first_name'].disabled = True
+             
 
         return form  
